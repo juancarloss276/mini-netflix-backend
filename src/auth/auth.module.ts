86e57c8f68ebaca87827c1257ea2884fc640cfa7
Juan Carlos Salamanca Gutierrez <juan.salamanca@oep.net>
2025-12-24 13:48:30 -0400

    fix: jwt strategy compatible

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -11,14 +11,16 @@ import { UsersModule } from '../users/users.module';
   imports: [
     UsersModule,
     PassportModule,
-    JwtModule.registerAsync({
+   /* JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
         secret: config.get<string>('JWT_SECRET')!,
         signOptions: { expiresIn: 3600 },
       }),
-    }),
+    }),*/
+
+    JwtModule.register({}),
   ],
   providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
